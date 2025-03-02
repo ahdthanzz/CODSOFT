6e4ad46820aaf35be2273689c1e1cca999c4aa0b
ahdthanzz <thansimahamed123@gmail.com>
2025-03-02 15:37:57 +0530

    project
    This For Number Addition

---
+++ NET22B145/NumberAddition/src/main/java/com/mycompany/numberaddition/NumberAddition.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.numberaddition;
+public class NumberAddition {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
